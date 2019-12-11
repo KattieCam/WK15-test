22c2cf14c2294b9306402ff27959d0077081c00c
Kattie <mr4529fy@go.minnstate.edu>
2019-12-11 17:39:22 -0600

    edited comment code

--- final.py
+++ final.py
@@ -5,19 +5,25 @@ taco_data = 'https://taco-1150.herokuapp.com/random/?full_taco=true'  # requesti
 response = requests.get(taco_data)
 
 if not response:
-    print('page you requested cannot be found')  # checking if data is usable
+    print('page you requested cannot be found')  # checking if data is available
     quit()
 
 response_json = response.json()  # this will bring back a dictionary of variables, of keys and ingredients
 
-print('printing the name of the seasoning:')
+print('printing the name of the seasoning:')  # returning name of seasoning
 print(response_json['seasoning']['name'])
 print('printing the recipe of the seasoning:')
-print(response_json['seasoning']['recipe'])
+print(response_json['seasoning']['recipe'])  # return recipe to seasoning
 
+
+# use this if you want to view all of the data / to see what the keys are
 """
-for key in response_json.keys(): # for use if needed to run all data
-    print(f'key = {key}')
-    print(response_json[key])
+for first_key in response_json.keys(): 
+    item = response_json[first_key]
+    for second_key in item.keys()
+    print(f'--------------------------------')
+    print(f'[{first_key}][{second_key}]')
+    print(f'--------------------------------')
+    print(item[second_key])
     print()
 """
